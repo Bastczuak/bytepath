13cb31ec40e15ab5cfa94991489ec3e3fc94b793
Sebastian Richter <sebastian.richter@paessler.com>
2021-04-22 15:26:13 +0200

    -setup pixel look.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
-use sdl2::pixels::Color;
 use sdl2::event::Event;
+use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 use std::time::Duration;
+
+const SCREEN_WIDTH: u32 = 480;
+const SCREEN_HEIGHT: u32 = 280;
 
 fn main() -> Result<(), String> {
   let sdl_context = sdl2::init()?;
   let sdl_video = sdl_context.video()?;
+  let display_mode = sdl_video.desktop_display_mode(0)?;
+  let factor = std::cmp::min(
+    display_mode.w / SCREEN_WIDTH as i32,
+    display_mode.h / SCREEN_HEIGHT as i32,
+  ) as u32;
+
   let sdl_window = sdl_video
-    .window("bytepath", 800, 600)
+    .window("bytepath", SCREEN_WIDTH * factor, SCREEN_HEIGHT * factor)
     .position_centered()
     .build()
     .map_err(|e| e.to_string())?;
 
-  let mut canvas = sdl_window
-    .into_canvas()
-    .build()
+  let mut canvas = sdl_window.into_canvas().build().map_err(|e| e.to_string())?;
+  canvas
+    .set_logical_size(SCREEN_WIDTH, SCREEN_HEIGHT)
     .map_err(|e| e.to_string())?;
-  canvas.set_draw_color(Color::RGB(255, 0, 0));
+  canvas.set_draw_color(Color::BLACK);
   canvas.clear();
   canvas.present();
 
@@ -25,7 +35,8 @@ fn main() -> Result<(), String> {
   'running: loop {
     for event in event_pump.poll_iter() {
       match event {
-        Event::Quit { .. } | Event::KeyDown {
+        Event::Quit { .. }
+        | Event::KeyDown {
           keycode: Some(Keycode::Escape),
           ..
         } => break 'running,
@@ -33,11 +44,14 @@ fn main() -> Result<(), String> {
       }
     }
 
+    canvas.set_draw_color(Color::BLACK);
     canvas.clear();
+
+    canvas.circle((SCREEN_WIDTH / 2) as i16, (SCREEN_HEIGHT / 2) as i16, 50, Color::WHITE)?;
     canvas.present();
+
     std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
   }
-
 
   Ok(())
 }
